6d8f1badeb190c520bc0c1169cd1b37a889ad16e
Julius Adebowale <juadebgabriel@gmail.com>
2022-11-25 08:03:18 -0600

    Fix

--- Index.js
+++ Index.js
@@ -122,34 +122,32 @@ function logIn(event) {
       .catch((error) => console.log("error", error));
   }
 }
-// function login ends here
 
-// function to pop up for top three students
-// var modal = document.getElementById("popup-modal");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-// var modalBtn = document.getElementById("modal-Btn");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-// var closeBtn = document.getElementsByClassName("closeBtn")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-// modalBtn.addEventListener("click", openmodal);
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
 
-// closeBtn.addEventListener("click", closemodal);
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
 
-// window.addEventListener("click", outsideclick);
-
-// function openmodal() {
-//   modal.style.display = "block";
-// }
-
-// function closemodal() {
-//   modal.style.display = "none";
-// }
-
-// function outsideclick(e) {
-//   if (e.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 // function for dashboard details
 function getDashApi() {
   const mySpin = document.querySelector(".pagemodal");
