e63c56dc52efe6d98d518e00354e8dcaa975e0f4
Julius Adebowale <juadebgabriel@gmail.com>
2023-01-17 07:44:07 -0600

    Fix

--- category.js
+++ category.js
@@ -1,131 +1,3 @@
-// FUNCTIONS FOR SIGNUP
-function signUp(event) {
-  event.preventDefault();
-  // calling the spinner on the button
-  const getSpin = document.getElementById("spin");
-  getSpin.style.display = "inline-block";
-
-  const getName = document.getElementById("name").value;
-  const getEmail = document.getElementById("email").value;
-  const getPassword = document.getElementById("password").value;
-  const getConfirmPassword = document.getElementById("confirmPassword").value;
-
-  if (
-    getName === "" ||
-    getEmail === "" ||
-    getPassword === "" ||
-    getConfirmPassword === ""
-  ) {
-    Swal.fire({
-      icon: "warning",
-      text: "All fields are required",
-      confirmButtonColor: "#2d85de",
-    });
-    getSpin.style.display = "none";
-  } else if (getConfirmPassword !== getPassword) {
-    Swal.fire({
-      icon: "warning",
-      text: "Password is incorrect",
-      confirmButtonColor: "#2d85de",
-    });
-  } else if (getPassword.lenght > 6) {
-    Swal.fire({
-      icon: "info",
-      text: "Password should not be more than 6 characters",
-      confirmButtonColor: "#2d85de",
-    });
-  } else {
-    const regData = new FormData();
-    regData.append("name", getName);
-    regData.append("email", getEmail);
-    regData.append("password", getPassword);
-    regData.append("password_confirmation", getConfirmPassword);
-
-    const regRequest = {
-      method: "POST",
-      body: regData,
-    };
-
-    const url =
-      "https://pluralcodesandbox.com/yorubalearning/api/register_admin";
-
-    fetch(url, regRequest)
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-        if (result.status === "success") {
-          Swal.fire({
-            icon: "success",
-            text: `${result.message}`,
-            confirmButtonColor: "#2d85de",
-          });
-          setTimeout(() => {
-            window.location.href = "index.html";
-          }, 3000);
-        } else {
-          Swal.fire({
-            icon: "info",
-            text: "Unsuccessful",
-            confirmButtonColor: "#2d85de",
-          });
-          getSpin.style.display = "none";
-        }
-      })
-      .catch((error) => console.log("error", error));
-  }
-}
-// X FUNCTIONS FOR SIGNUP
-
-// FUNCTIONS FOR SIGNIN
-function signIn(event) {
-  event.preventDefault();
-  const getSpin = document.getElementById("spin");
-  getSpin.style.display = "inline-block";
-
-  const getEmail = document.getElementById("email").value;
-  const getPassword = document.getElementById("password").value;
-
-  if (getEmail === "" || getPassword === "") {
-    Swal.fire({
-      icon: "info",
-      text: "All fields are required",
-      confirmButtonColor: "#2d85de",
-    });
-    getSpin.style.display = "none";
-  } else {
-    const logData = new FormData();
-    logData.append("email", getEmail);
-    logData.append("password", getPassword);
-
-    const logReg = {
-      method: "POST",
-      body: logData,
-    };
-    const url = "https://pluralcodesandbox.com/yorubalearning/api/admin_login";
-
-    fetch(url, logReg)
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-        localStorage.setItem("adminlogin", JSON.stringify(result));
-        const myDetails = localStorage.getItem("adminlogin");
-        const theDetails = JSON.parse(myDetails);
-        if (theDetails.hasOwnProperty("email")) {
-          window.location.href = "/Dashbaord.html";
-        } else {
-          Swal.fire({
-            icon: "info",
-            text: "Unsuccessful",
-            confirmButtonColor: "#2d85de",
-          });
-          getSpin.style.display = "none";
-        }
-      })
-      .catch((error) => console.log("error", error));
-  }
-}
-// FUNCTIONS FOR SIGNIN
-
 // GET THE MODAL
 var modal = document.getElementById("myModal");
 
@@ -438,6 +310,21 @@ function closeCatModal() {
   pop.style.display = "none";
 }
 // FUNCTION FOR CLOSING UPDATE CATEGORY
+function changeimage() {
+  let change = document.querySelector(".hide-me");
+  change.style.display = "block";
+
+  let flex = document.querySelector(".show-me");
+  flex.style.display = "none";
+}
+//FUNCTION FOR CLOSE UPDATE BY CLICKING ANYWHERE ON THE WINDOW
+window.onclick = function outsideClick(e) {
+  const pop = document.querySelector(".updateform");
+  if (e.target == pop) {
+    pop.style.display = "none";
+  }
+};
+//FUNCTION FOR CLOSE UPDATE BY CLICKING ANYWHERE ON THE WINDOW
 
 // FUNCTION FOR UPDATE CATEGORY
 function UpdateCategory(event) {
